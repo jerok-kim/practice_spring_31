f6a379377454075c8824550376171d0db5afa290
jerok <jerok.kim@gmail.com>
2023-05-30 14:18:34 +0900

    practice spring

---
+++ src/main/java/kim/jerok/practice_spring_31/PracticeSpring31Application.java
@@ -0,0 +1,13 @@
+package kim.jerok.practice_spring_31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpring31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpring31Application.class, args);
+    }
+
+}
---
+++ src/test/java/kim/jerok/practice_spring_31/PracticeSpring31ApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.jerok.practice_spring_31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpring31ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
