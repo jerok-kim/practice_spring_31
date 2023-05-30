4b2095790095253fb8a8423c911cd4d3692ffd2e
jerok <jerok.kim@gmail.com>
2023-05-30 15:00:35 +0900

    practice spring

--- src/test/java/kim/jerok/practice_spring_31/PracticeSpring31ApplicationTests.java
+++ src/test/java/kim/jerok/practice_spring_31/PracticeSpring31ApplicationTests.java
@@ -1,13 +1,39 @@
 package kim.jerok.practice_spring_31;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import kim.jerok.practice_spring_31.entity.Hello;
+import kim.jerok.practice_spring_31.entity.QHello;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class PracticeSpring31ApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
